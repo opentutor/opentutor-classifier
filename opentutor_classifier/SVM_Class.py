bb5c1b1aa3bbdc477527b0b561a38ef1f5d7edec
larry kirschner <larrykirschner@gmail.com>
2020-06-03 13:24:55 -0700

    fixes: lint errors

--- opentutor_classifier/SVM_Class.py
+++ opentutor_classifier/SVM_Class.py
@@ -25,9 +25,7 @@ from nltk import pos_tag
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from sklearn.preprocessing import LabelEncoder
-from collections import defaultdict
 from nltk.corpus import wordnet as wn
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import model_selection, svm
 from sklearn.metrics import accuracy_score
 import pickle
