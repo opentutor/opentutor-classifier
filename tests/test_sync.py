0ff02d72ac2e5fe7442824c84d5d9570c814b1c7
kcarr <kcarr@ict.usc.edu>
2020-07-24 12:21:22 -0700

    WIP: add sync that downloads lesson training data into csv and config

---
+++ tests/test_sync.py
@@ -0,0 +1,72 @@
+# import os
+# from os import path
+# from opentutor_classifier.sync import sync, sync_config, sync_training
+# import pytest
+# from . import fixture_path
+# import responses
+
+
+# @pytest.fixture(scope="module")
+# def data_root() -> str:
+#     return fixture_path("data")
+
+
+# def __sync(tmpdir, lesson: str, url: str, data_root: str):
+#     output_dir = os.path.join(tmpdir.mkdir("test"), data_root, lesson)
+#     sync(lesson, url, output_dir)
+#     return output_dir
+
+
+# def __sync_training(tmpdir, lesson: str, url: str, data_root: str):
+#     output_dir = os.path.join(tmpdir.mkdir("test"), data_root, lesson)
+#     sync_training(lesson, url, output_dir)
+#     return output_dir
+
+
+# def __sync_config(tmpdir, lesson: str, url: str, data_root: str):
+#     output_dir = os.path.join(tmpdir.mkdir("test"), data_root, lesson)
+#     sync_config(lesson, url, output_dir)
+#     return output_dir
+
+
+# @responses.activate
+# def test_syncs_training_data_for_q1(tmpdir, data_root: str):
+#     responses.add(
+#         responses.POST,
+#         "https://dev-opentutor.pal3.org/grading-api",
+#         json={
+#             "data": {"lessonTrainingData": "exp_num,text,label\n0,peer pressure,Good"}
+#         },
+#         status=200,
+#     )
+#     output_dir = __sync_training(
+#         tmpdir, "q1", "https://dev-opentutor.pal3.org/grading-api", data_root
+#     )
+#     assert path.exists(path.join(output_dir, "training.csv"))
+#     with open(path.join(output_dir, "training.csv")) as f:
+#         assert "exp_num,text,label\n" in f.read()
+
+
+# @responses.activate
+# def test_syncs_config_data_for_q1(tmpdir, data_root: str):
+#     responses.add(
+#         responses.POST,
+#         "https://dev-opentutor.pal3.org/grading-api",
+#         json={
+#             "data": {
+#                 "lesson": {
+#                     "question": "What are the challenges to demonstrating integrity in a group?"
+#                 }
+#             }
+#         },
+#         status=200,
+#     )
+#     output_dir = __sync_config(
+#         tmpdir, "q1", "https://dev-opentutor.pal3.org/grading-api", data_root
+#     )
+#     assert path.exists(path.join(output_dir, "config.yaml"))
+#     with open(path.join(output_dir, "config.yaml")) as f:
+#         assert (
+#             f.read()
+#             == 'question: "What are the challenges to demonstrating integrity in a group?"'
+#         )
