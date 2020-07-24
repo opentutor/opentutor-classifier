92d6ca6de2973617c5eddf529788aaf43c403aef
larry kirschner <larrykirschner@gmail.com>
2020-07-24 13:09:29 -0700

    fixes: pytest sync tests:

--- tests/test_sync.py
+++ tests/test_sync.py
@@ -1,36 +1,37 @@
 import os
 from os import path
 from opentutor_classifier.sync import sync, sync_config, sync_training
-import pytest
-from . import fixture_path
+
+# import pytest
+# from . import fixture_path
 import responses
 
 
-@pytest.fixture(scope="module")
-def data_root() -> str:
-    return fixture_path("data")
+# @pytest.fixture(scope="module")
+# def data_root() -> str:
+#     return fixture_path("data")
 
 
-def __sync(tmpdir, lesson: str, url: str, data_root: str):
-    output_dir = os.path.join(tmpdir.mkdir("test"), data_root, lesson)
+def __sync(tmpdir, lesson: str, url: str):
+    output_dir = os.path.join(tmpdir.mkdir("test"), lesson)
     sync(lesson, url, output_dir)
     return output_dir
 
 
-def __sync_training(tmpdir, lesson: str, url: str, data_root: str):
-    output_dir = os.path.join(tmpdir.mkdir("test"), data_root, lesson)
+def __sync_training(tmpdir, lesson: str, url: str):
+    output_dir = os.path.join(tmpdir.mkdir("test"), lesson)
     sync_training(lesson, url, output_dir)
     return output_dir
 
 
-def __sync_config(tmpdir, lesson: str, url: str, data_root: str):
-    output_dir = os.path.join(tmpdir.mkdir("test"), data_root, lesson)
+def __sync_config(tmpdir, lesson: str, url: str):
+    output_dir = os.path.join(tmpdir.mkdir("test"), lesson)
     sync_config(lesson, url, output_dir)
     return output_dir
 
 
 @responses.activate
-def test_syncs_training_data_for_q1(tmpdir, data_root: str):
+def test_syncs_training_data_for_q1(tmpdir):
     responses.add(
         responses.POST,
         "https://dev-opentutor.pal3.org/grading-api",
@@ -40,7 +41,7 @@ def test_syncs_training_data_for_q1(tmpdir, data_root: str):
         status=200,
     )
     output_dir = __sync_training(
-        tmpdir, "q1", "https://dev-opentutor.pal3.org/grading-api", data_root
+        tmpdir, "q1", "https://dev-opentutor.pal3.org/grading-api"
     )
     assert path.exists(path.join(output_dir, "training.csv"))
     with open(path.join(output_dir, "training.csv")) as f:
@@ -48,7 +49,7 @@ def test_syncs_training_data_for_q1(tmpdir, data_root: str):
 
 
 @responses.activate
-def test_syncs_config_data_for_q1(tmpdir, data_root: str):
+def test_syncs_config_data_for_q1(tmpdir):
     responses.add(
         responses.POST,
         "https://dev-opentutor.pal3.org/grading-api",
@@ -62,7 +63,7 @@ def test_syncs_config_data_for_q1(tmpdir, data_root: str):
         status=200,
     )
     output_dir = __sync_config(
-        tmpdir, "q1", "https://dev-opentutor.pal3.org/grading-api", data_root
+        tmpdir, "q1", "https://dev-opentutor.pal3.org/grading-api"
     )
     assert path.exists(path.join(output_dir, "config.yaml"))
     with open(path.join(output_dir, "config.yaml")) as f:
