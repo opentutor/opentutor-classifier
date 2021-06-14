25b3c68cc1cdfb3fa18824a01a09a081c87f97b0
larry kirschner <larrykirschner@gmail.com>
2021-06-14 17:53:17 -0400

    example

--- opentutor_classifier_api/tests/test_healthcheck.py
+++ opentutor_classifier_api/tests/test_healthcheck.py
@@ -4,11 +4,39 @@
 #
 # The full terms of this copyright and license should always be found in the root directory of this software deliverable as "license.txt" and if these terms are not found with this software, please contact the USC Stevens Center for the full license.
 #
+from contextlib import contextmanager
 import pytest
 import responses
 import json
 from . import fixture_path
 import os
+
+
+@contextmanager
+def _mock_healthchecks(
+    gql_fixture_name: str,
+    gql_status=200,
+    admin_status=200,
+    home_status=200,
+    tutor_status=200,
+) -> None:
+    rsps = responses.RequestsMock()
+    try:
+        rsps.start()
+        with open(
+            fixture_path(os.path.join("graphql", f"{gql_fixture_name}.json"))
+        ) as f:
+            data = json.load(f)
+            rsps.add(
+                responses.POST, "http://graphql/graphql", json=data, status=gql_status
+            )
+        rsps.add(responses.HEAD, "http://admin", status=admin_status)
+        rsps.add(responses.HEAD, "http://home", status=home_status)
+        rsps.add(responses.HEAD, "http://tutor", status=tutor_status)
+        # responses.add(responses.GET, "http://training/ping", status=200)
+        yield None
+    finally:
+        rsps.stop()
 
 
 @responses.activate
@@ -29,27 +57,20 @@ def test_healthcheck_returns_all_statuses(client, message, status):
     assert len(res.json.get("services")) == 5
 
 
-@responses.activate
+@pytest.mark.only
 @pytest.mark.parametrize(
     "message,status",
     [("pong!", "success")],
 )
 def test_200_if_all_healthy(client, message, status):
-    with open(fixture_path("graphql/admin_ok.json")) as f:
-        data = json.load(f)
-    responses.add(responses.POST, "http://graphql/graphql", json=data, status=200)
-    responses.add(responses.HEAD, "http://admin", status=200)
-    responses.add(responses.HEAD, "http://home", status=200)
-    responses.add(responses.HEAD, "http://tutor", status=200)
-    # responses.add(responses.GET, "http://training/ping", status=200)
-
-    res = client.get("/classifier/healthcheck/")
-    assert res.json["services"]["graphql"]["status"] == 200
-    assert res.json["services"]["admin"]["status"] == 200
-    assert res.json["services"]["home"]["status"] == 200
-    assert res.json["services"]["tutor"]["status"] == 200
-    # assert res.json["services"]["training"]["status"] == 200
-    assert res.status_code == 200
+    with _mock_healthchecks("admin_ok"):
+        res = client.get("/classifier/healthcheck/")
+        assert res.json["services"]["graphql"]["status"] == 200
+        assert res.json["services"]["admin"]["status"] == 200
+        assert res.json["services"]["home"]["status"] == 200
+        assert res.json["services"]["tutor"]["status"] == 200
+        # assert res.json["services"]["training"]["status"] == 200
+        assert res.status_code == 200
 
 
 @responses.activate
