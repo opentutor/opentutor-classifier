ec27623b7953c5892c7a9669702e13492d0ad679
aaronshiel <ashiel408@gmail.com>
2023-05-18 11:18:42 -0700

    training_data lambda function

---
+++ src/functions/training_data.py
@@ -0,0 +1,15 @@
+import json
+from serverless_modules.utils import create_json_response
+from serverless_modules.train_job.api import fetch_training_data
+
+
+def handler(event, context):
+    print(json.dumps(event))
+    lesson_id = event["pathParameters"]["lesson_id"]
+    data = fetch_training_data(lesson_id)
+    data_csv = data.data.to_csv(index=False)
+    extra_headers = {
+        "Content-Disposition": "attachment; filename=mentor.csv",
+        "Content-type": "text/csv",
+    }
+    return create_json_response(200, data_csv, event, extra_headers)
