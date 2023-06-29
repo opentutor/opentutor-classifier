d817a3ca472f5d605c2f4066c95878b9fb63f329
dan100110 <daniel.auerbach@gmail.com>
2023-06-29 15:39:20 -0700

    wip

---
+++ opentutor_classifier/opentutor_classifier/openai/__init__.py
@@ -0,0 +1,26 @@
+#
+# This software is Copyright ©️ 2020 The University of Southern California. All Rights Reserved.
+# Permission to use, copy, modify, and distribute this software and its documentation for educational, research and non-profit purposes, without fee, and without a written agreement is hereby granted, provided that the above copyright notice and subject to the full license file found in the root of this software deliverable. Permission to make commercial use of this software may be obtained by contacting:  USC Stevens Center for Innovation University of Southern California 1150 S. Olive Street, Suite 2300, Los Angeles, CA 90115, USA Email: accounting@stevens.usc.edu
+#
+# The full terms of this copyright and license should always be found in the root directory of this software deliverable as "license.txt" and if these terms are not found with this software, please contact the USC Stevens Center for the full license.
+#
+
+from dataclasses import dataclass
+
+@dataclass
+class OpenAICall:
+    system_assignment: str
+    user_answer: dict
+    user_template: str
+    user_guardrails: str
+
+
+@dataclass
+class Concept:
+    is_known: bool
+    confidence: float
+    justification: str
+
+@dataclass
+class OpenAIResult:
+    
