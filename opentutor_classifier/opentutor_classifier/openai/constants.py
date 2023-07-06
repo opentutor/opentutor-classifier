cc9ad5594ec71e3f09a1dff7b700adc9a4e7602a
dan100110 <daniel.auerbach@gmail.com>
2023-07-06 15:44:42 -0700

    added license.

--- opentutor_classifier/opentutor_classifier/openai/constants.py
+++ opentutor_classifier/opentutor_classifier/openai/constants.py
@@ -1,3 +1,10 @@
+#
+# This software is Copyright ©️ 2020 The University of Southern California. All Rights Reserved.
+# Permission to use, copy, modify, and distribute this software and its documentation for educational, research and non-profit purposes, without fee, and without a written agreement is hereby granted, provided that the above copyright notice and subject to the full license file found in the root of this software deliverable. Permission to make commercial use of this software may be obtained by contacting:  USC Stevens Center for Innovation University of Southern California 1150 S. Olive Street, Suite 2300, Los Angeles, CA 90115, USA Email: accounting@stevens.usc.edu
+#
+# The full terms of this copyright and license should always be found in the root directory of this software deliverable as "license.txt" and if these terms are not found with this software, please contact the USC Stevens Center for the full license.
+#
+
 SYSTEM_ASSIGNMENT = 'The user provided a list of one or more "answers" to a tutoring question. The answers are provided in JSON format. You are a tutor who is evaluating if the answer is sufficient to show that the user knows a one or more "concepts" which will be labeled "concept_1" to "concept_N". For each answer you evaluate, you must also express how confident you are in your evaluation of how well the answer shows knowledge of each concept. You will also provide a brief justification.'
 CONCEPT_HEADER = "These are the concepts for this lesson."
 ANSWER_HEADER = "These are my answers provided in JSON to be evaluated."
