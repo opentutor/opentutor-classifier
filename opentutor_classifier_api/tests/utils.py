aefdc8105afaee3aecbb7edabd0da86bd57017ed
dan100110 <daniel.auerbach@gmail.com>
2023-10-13 14:27:08 -0700

    forgot to add functionality to additional implementation of the dao

--- opentutor_classifier_api/tests/utils.py
+++ opentutor_classifier_api/tests/utils.py
@@ -89,6 +89,9 @@ class _TestDataDao(DataDao):
     def load_pickle(self, ref: ModelRef) -> Any:
         return self.dao.load_pickle(ref)
 
+    def load_ground_truth(self, ref: ModelRef) -> Any:
+        return self.file_dao.load_ground_truth(ref)
+
     def trained_model_exists(self, ref: ModelRef) -> bool:
         return self.dao.trained_model_exists(ref)
 
