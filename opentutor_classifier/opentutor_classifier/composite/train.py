e7324c8a6ca53d94b8f8f3cb4c4caa267f713722
dan100110 <daniel.auerbach@gmail.com>
2023-10-17 12:51:24 -0700

    updated composite classifier to train openai groundtruths as well as lr2 models.

--- opentutor_classifier/opentutor_classifier/composite/train.py
+++ opentutor_classifier/opentutor_classifier/composite/train.py
@@ -15,23 +15,29 @@ from opentutor_classifier import (
     TrainingResult,
 )
 from opentutor_classifier.lr2.train import LRAnswerClassifierTraining
+from opentutor_classifier.openai.train import OpenAIAnswerClassifierTraining
 
 
 class CompositeAnswerClassifierTraining(AnswerClassifierTraining):
 
     lr_training: AnswerClassifierTraining = LRAnswerClassifierTraining()
+    openai_training: AnswerClassifierTraining = OpenAIAnswerClassifierTraining()
 
     def configure(self, config: TrainingConfig) -> "AnswerClassifierTraining":
         self.lr_training = self.lr_training.configure(config)
+        self.openai_training = self.openai_training.configure(config)
         return self
 
     def train(
         self, train_input: TrainingInput, dao: DataDao, developer_mode: bool = False
     ) -> TrainingResult:
+        self.openai_training.train(train_input, dao, developer_mode)
         return self.lr_training.train(train_input, dao, developer_mode)
 
     def train_default(self, data: pd.DataFrame, dao: DataDao) -> TrainingResult:
         return self.lr_training.train_default(data, dao)
 
     def upload_model(self, s3: Any, lesson: str, s3_bucket: str):
-        return self.lr_training.upload_model(s3, lesson, s3_bucket)
+        self.openai_training.upload_model(s3, lesson, s3_bucket)
+        self.lr_training.upload_model(s3, lesson, s3_bucket)
+        return
